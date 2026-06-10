93fe341f03a51402e3d39f3eb1b947ff129b794f
Liam <46268350+LMLiam@users.noreply.github.com>
2026-06-10 18:01:14 +0100

    feat(core): add block nbt position helpers (#116)
    Co-authored-by: LMLiam <46268350+TheRealEmissions@users.noreply.github.com>

---
+++ modules/core/src/test/kotlin/io/github/lmliam/kotventure/core/nbt/BlockPosDslTest.kt
@@ -0,0 +1,63 @@
+package io.github.lmliam.kotventure.core.nbt
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import net.kyori.adventure.text.BlockNBTComponent
+
+class BlockPosDslTest :
+    StringSpec(
+        {
+            "builds an absolute block position from integer coordinates" {
+                val pos = blockPos(1, 64, -3)
+                val expected =
+                    BlockNBTComponent.WorldPos.worldPos(
+                        BlockNBTComponent.WorldPos.Coordinate.absolute(1),
+                        BlockNBTComponent.WorldPos.Coordinate.absolute(64),
+                        BlockNBTComponent.WorldPos.Coordinate.absolute(-3),
+                    )
+
+                pos shouldBe expected
+                pos.asString() shouldBe "1 64 -3"
+            }
+
+            "builds a relative block position from integer offsets" {
+                val pos = relativeBlockPos(1, 2, -2)
+                val expected =
+                    BlockNBTComponent.WorldPos.worldPos(
+                        BlockNBTComponent.WorldPos.Coordinate.relative(1),
+                        BlockNBTComponent.WorldPos.Coordinate.relative(2),
+                        BlockNBTComponent.WorldPos.Coordinate.relative(-2),
+                    )
+
+                pos shouldBe expected
+                pos.asString() shouldBe "~1 ~2 ~-2"
+            }
+
+            "builds a relative block position from zero offsets by default" {
+                val pos = relativeBlockPos()
+                val expected =
+                    BlockNBTComponent.WorldPos.worldPos(
+                        BlockNBTComponent.WorldPos.Coordinate.relative(0),
+                        BlockNBTComponent.WorldPos.Coordinate.relative(0),
+                        BlockNBTComponent.WorldPos.Coordinate.relative(0),
+                    )
+
+                pos shouldBe expected
+                pos.asString() shouldBe "~0 ~0 ~0"
+            }
+
+            "parses a block position from a coordinate string" {
+                val pos = blockPos("~1 ~2 ~3")
+
+                pos shouldBe BlockNBTComponent.Pos.fromString("~1 ~2 ~3")
+                pos.asString() shouldBe "~1 ~2 ~3"
+            }
+
+            "throws when parsing an invalid coordinate string" {
+                shouldThrow<IllegalArgumentException> {
+                    blockPos("not coordinates")
+                }
+            }
+        },
+    )
