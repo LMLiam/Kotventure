89a2591cf347cfb55c72e414c3bf8d3fd84518a7
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-06-10 18:14:58 +0100

    test(core): cover invalid key helpers

--- modules/core/src/test/kotlin/io/github/lmliam/kotventure/core/key/KeyDslTest.kt
+++ modules/core/src/test/kotlin/io/github/lmliam/kotventure/core/key/KeyDslTest.kt
@@ -1,7 +1,9 @@
 package io.github.lmliam.kotventure.core.key
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import net.kyori.adventure.key.InvalidKeyException
 import net.kyori.adventure.key.Key
 
 class KeyDslTest :
@@ -38,6 +40,24 @@ class KeyDslTest :
                 adventureKey shouldBe Key.key("minecraft", "stone")
                 adventureKey.namespace() shouldBe "minecraft"
                 adventureKey.value() shouldBe "stone"
+            }
+
+            "throws when building a key with an invalid namespace" {
+                shouldThrow<InvalidKeyException> {
+                    key("not valid", "messages")
+                }
+            }
+
+            "throws when building a key with infix invalid value" {
+                shouldThrow<InvalidKeyException> {
+                    "kotventure" namespace "Not Valid"
+                }
+            }
+
+            "throws when parsing an invalid key string" {
+                shouldThrow<InvalidKeyException> {
+                    "not valid:messages".asKey()
+                }
             }
         },
     )
