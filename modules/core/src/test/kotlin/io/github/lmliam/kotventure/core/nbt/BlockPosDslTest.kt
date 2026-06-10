9cfd3013df0ee15555033a1f34b6b4f70cec9fa8
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-06-10 17:38:38 +0100

    feat(core): add block nbt position helpers

---
+++ modules/core/src/test/kotlin/io/github/lmliam/kotventure/core/nbt/BlockPosDslTest.kt
@@ -0,0 +1,41 @@
+package io.github.lmliam.kotventure.core.nbt
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import net.kyori.adventure.text.BlockNBTComponent
+
+class BlockPosDslTest :
+    StringSpec(
+        {
+            "builds an absolute block position from integer coordinates" {
+                val pos = blockPos(1, 64, -3)
+
+                pos shouldBe
+                    BlockNBTComponent.WorldPos.worldPos(
+                        BlockNBTComponent.WorldPos.Coordinate.absolute(1),
+                        BlockNBTComponent.WorldPos.Coordinate.absolute(64),
+                        BlockNBTComponent.WorldPos.Coordinate.absolute(-3),
+                    )
+                pos.asString() shouldBe "1 64 -3"
+            }
+
+            "builds a relative block position from integer offsets" {
+                val pos = relativeBlockPos(1, 0, -2)
+
+                pos shouldBe
+                    BlockNBTComponent.WorldPos.worldPos(
+                        BlockNBTComponent.WorldPos.Coordinate.relative(1),
+                        BlockNBTComponent.WorldPos.Coordinate.relative(0),
+                        BlockNBTComponent.WorldPos.Coordinate.relative(-2),
+                    )
+                pos.asString() shouldBe "~1 ~0 ~-2"
+            }
+
+            "parses a block position from a coordinate string" {
+                val pos = blockPos("~1 ~2 ~3")
+
+                pos shouldBe BlockNBTComponent.Pos.fromString("~1 ~2 ~3")
+                pos.asString() shouldBe "~1 ~2 ~3"
+            }
+        },
+    )
