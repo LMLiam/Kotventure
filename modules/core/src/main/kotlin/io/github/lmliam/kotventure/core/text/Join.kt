abca147cd2bb9ae719537bd4865a5c1dd6c02552
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-06-12 17:01:21 +0100

    feat(core): extend join DSL to support Array<ComponentLike> and Iterable<ComponentLike>

--- modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/text/Join.kt
+++ modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/text/Join.kt
@@ -1,9 +1,15 @@
 package io.github.lmliam.kotventure.core.text
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.ComponentLike
 
 /**
  * Joins this sequence of components into a single Adventure [Component], configured by [init].
  */
-public fun Iterable<Component>.join(init: JoinScope.() -> Unit = {}): Component =
+public fun <T : ComponentLike> Array<T>.join(init: JoinScope.() -> Unit = {}): Component = this.asIterable().join(init)
+
+/**
+ * Joins this sequence of components into a single Adventure [Component], configured by [init].
+ */
+public fun <T : ComponentLike> Iterable<T>.join(init: JoinScope.() -> Unit = {}): Component =
     Component.join(JoinBuilder().apply(init).build(), this)
