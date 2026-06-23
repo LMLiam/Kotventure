e864da4aa67722867d582dd54ee5c117ed3e23d6
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-06-23 18:13:32 +0100

    chore(core): remove MiniMessage tag APIs and related interfaces

--- modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/minimessage/MiniMessageTagLookup.kt
+++
@@ -1,8 +0,0 @@
-package io.github.lmliam.kotventure.core.minimessage
-
-import io.github.lmliam.kotventure.core.registry.AdventureDsl
-
-/**
- * Returns the MiniMessage tag provider registered as [name], or null when none exists.
- */
-public fun miniMessageTag(name: String): MiniMessageTagProvider? = AdventureDsl.miniMessageTag(name)
