4ed5ef2b0a215c94c8266b4787d911d9b9fc84a8
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-06-23 18:13:23 +0100

    chore(core): remove unused `Key` extension functions

--- modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/key/Key.kt
+++ modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/key/Key.kt
@@ -12,19 +12,3 @@ public fun key(
     namespace: String,
     value: String,
 ): Key = Key.key(namespace, value)
-
-/**
- * Builds an Adventure [Key] using this string as the namespace and [value] as the value.
- *
- * @throws InvalidKeyException when this namespace or [value] is not valid for an Adventure key.
- */
-public infix fun String.namespace(value: String): Key = Key.key(this, value)
-
-/**
- * Parses this string as an Adventure [Key].
- *
- * Bare values use Adventure's default `minecraft` namespace.
- *
- * @throws InvalidKeyException when this string is not valid for an Adventure key.
- */
-public fun String.asKey(): Key = Key.key(this)
