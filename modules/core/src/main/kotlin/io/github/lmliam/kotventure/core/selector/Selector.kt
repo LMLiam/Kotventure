9b5b945c7d172f5c988b84d3fa878a4fb451d784
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-06-23 18:14:04 +0100

    chore(core): refactor `SelectorComponentBuilder` to use `buildTextComponent` and add nested selector support in `ComponentScope`

--- modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/selector/Selector.kt
+++ modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/selector/Selector.kt
@@ -1,5 +1,7 @@
 package io.github.lmliam.kotventure.core.selector
 
+import io.github.lmliam.kotventure.core.component.ComponentScope
+import io.github.lmliam.kotventure.core.component.addChild
 import net.kyori.adventure.text.Component
 
 /**
@@ -8,4 +10,19 @@ import net.kyori.adventure.text.Component
 public fun selector(
     pattern: String,
     init: SelectorScope.() -> Unit = {},
+): Component = buildSelectorComponent(pattern, init)
+
+internal fun buildSelectorComponent(
+    pattern: String,
+    init: SelectorScope.() -> Unit = {},
 ): Component = SelectorComponentBuilder(pattern).apply(init).build()
+
+/**
+ * Appends a nested selector child with [pattern] as its selector pattern.
+ */
+public fun ComponentScope.selector(
+    pattern: String,
+    init: SelectorScope.() -> Unit = {},
+) {
+    addChild(buildSelectorComponent(pattern, init))
+}
