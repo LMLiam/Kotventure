f18b0a0ab93926de2461825ec9f1d5733c362668
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-06-23 18:13:02 +0100

    chore(core): remove AnimationDriver and related APIs

--- modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/animation/AnimationDriverLookup.kt
+++
@@ -1,8 +0,0 @@
-package io.github.lmliam.kotventure.core.animation
-
-import io.github.lmliam.kotventure.core.registry.AdventureDsl
-
-/**
- * Returns the animation driver registered as [name], or null when none exists.
- */
-public fun animationDriver(name: String): AnimationDriver? = AdventureDsl.animationDriver(name)
