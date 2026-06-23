cc510e39548f6f4bae63bbcd9f3f66fe1bf19409
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-06-23 18:18:48 +0100

    feat(compilation): add DSL compilation utilities and assertion for compilation errors

---
+++ modules/minimessage/src/test/kotlin/io/github/lmliam/kotventure/minimessage/GeneratedDslCompilation.kt
@@ -0,0 +1,55 @@
+package io.github.lmliam.kotventure.minimessage
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import io.kotest.matchers.shouldBe
+import net.kyori.adventure.text.Component
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+
+@OptIn(ExperimentalCompilerApi::class)
+internal fun compileGeneratedDsl(source: String): Component {
+    val generated =
+        """
+        package io.github.lmliam.kotventure.minimessage.generated
+
+        import io.github.lmliam.kotventure.core.component.component
+        import io.github.lmliam.kotventure.core.event.click
+        import io.github.lmliam.kotventure.core.event.hover
+        import io.github.lmliam.kotventure.core.key.key
+        import io.github.lmliam.kotventure.core.keybind.keybind
+        import io.github.lmliam.kotventure.core.nbt.blockNbt
+        import io.github.lmliam.kotventure.core.nbt.blockPos
+        import io.github.lmliam.kotventure.core.nbt.entityNbt
+        import io.github.lmliam.kotventure.core.nbt.storageNbt
+        import io.github.lmliam.kotventure.core.objectcomponent.display
+        import io.github.lmliam.kotventure.core.objectcomponent.sprite
+        import io.github.lmliam.kotventure.core.objectcomponent.head
+        import io.github.lmliam.kotventure.core.score.score
+        import io.github.lmliam.kotventure.core.selector.selector
+        import io.github.lmliam.kotventure.core.text.text
+        import io.github.lmliam.kotventure.core.translatable.translatable
+        import net.kyori.adventure.nbt.api.BinaryTagHolder
+        import net.kyori.adventure.text.Component
+        import net.kyori.adventure.text.format.NamedTextColor
+        import net.kyori.adventure.text.format.TextColor
+        import net.kyori.adventure.text.format.ShadowColor
+        import net.kyori.adventure.text.format.TextDecoration
+        import java.util.UUID
+
+        fun renderGenerated(): Component =
+        ${source.prependIndent("    ")}
+        """.trimIndent()
+
+    val result =
+        KotlinCompilation()
+            .apply {
+                inheritClassPath = true
+                sources = listOf(SourceFile.kotlin("GeneratedDsl.kt", generated))
+            }.compile()
+
+    result.exitCode shouldBe KotlinCompilation.ExitCode.OK
+
+    val generatedClass =
+        result.classLoader.loadClass("io.github.lmliam.kotventure.minimessage.generated.GeneratedDslKt")
+    return generatedClass.getMethod("renderGenerated").invoke(null) as Component
+}
---
+++ modules/minimessage/src/test/kotlin/io/github/lmliam/kotventure/test/compilation/Assertions.kt
@@ -0,0 +1,26 @@
+package io.github.lmliam.kotventure.test.compilation
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+
+@OptIn(ExperimentalCompilerApi::class)
+internal fun assertDoesNotCompile(
+    fileName: String,
+    source: String,
+    vararg expectedMessages: String,
+) {
+    val result =
+        KotlinCompilation()
+            .apply {
+                inheritClassPath = true
+                sources = listOf(SourceFile.kotlin(fileName, source))
+            }.compile()
+
+    result.exitCode shouldBe KotlinCompilation.ExitCode.COMPILATION_ERROR
+    expectedMessages.forEach { expectedMessage ->
+        result.messages shouldContain expectedMessage
+    }
+}
