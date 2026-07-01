5758c8a40353711ae96982415fb2d28cf3b394ce
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-07-02 00:37:40 +0100

    style(core): fix indentation in selector token logic

--- modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/selector/SelectorToken.kt
+++ modules/core/src/main/kotlin/io/github/lmliam/kotventure/core/selector/SelectorToken.kt
@@ -6,9 +6,9 @@ package io.github.lmliam.kotventure.core.selector
  */
 internal fun Char.isAllowedInUnquotedSelectorToken(): Boolean =
     this in '0'..'9' ||
-        this in 'A'..'Z' ||
-        this in 'a'..'z' ||
-        this == '_' ||
-        this == '-' ||
-        this == '.' ||
-        this == '+'
+            this in 'A'..'Z' ||
+            this in 'a'..'z' ||
+            this == '_' ||
+            this == '-' ||
+            this == '.' ||
+            this == '+'
