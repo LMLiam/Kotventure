9edd591c3b2929b111c95f037f9e65ea6092ac1e
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-07-21 23:46:05 +0100

    refactor(project): reformat code

--- modules/coroutines/src/test/kotlin/io/github/lmliam/kotventure/coroutines/prompt/KitPrompt.kt
+++ modules/coroutines/src/test/kotlin/io/github/lmliam/kotventure/coroutines/prompt/KitPrompt.kt
@@ -2,8 +2,10 @@ package io.github.lmliam.kotventure.coroutines.prompt
 
 import io.github.lmliam.kotventure.core.text.text
 
-internal object KitPrompt : Prompt<Kit>({
-    text("Choose a kit: ")
-    option(Kit.ARCHER) { text(Kit.ARCHER.label) }
-    option(Kit.MAGE) { text(Kit.MAGE.label) }
-})
+internal object KitPrompt : Prompt<Kit>(
+    {
+        text("Choose a kit: ")
+        option(Kit.ARCHER) { text(Kit.ARCHER.label) }
+        option(Kit.MAGE) { text(Kit.MAGE.label) }
+    },
+)
